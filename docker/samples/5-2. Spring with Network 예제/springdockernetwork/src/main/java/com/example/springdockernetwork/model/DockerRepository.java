db4a83e5bf3ac9e9d9c16baeb834f98fa0bc9450
good593 <goodwon593@gmail.com>
2024-05-02 16:44:20 +0900

    remove docker

--- docker/samples/5-2. Spring with Network 예제/springdockernetwork/src/main/java/com/example/springdockernetwork/model/DockerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.springdockernetwork.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DockerRepository extends JpaRepository<DockerEntity, Long> {
-    
-}
